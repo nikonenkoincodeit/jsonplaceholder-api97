8f962881d732d56d5c01fc9f354a550255c6ffe3
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-08-11 21:05:25 +0300

    commit get userid

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,26 @@
-import { getData } from "../api";
-import { usersWrapperEl, loader } from "../refs";
-import { addMarkup } from "../helpers";
-import { markupUsers } from "../markup";
+import { getData } from '../api';
+import { usersWrapperEl, loader } from '../refs';
+import { addMarkup } from '../helpers';
+import { markupUsers } from '../markup';
 
 async function onLoaded() {
   try {
-    loader.classList.remove("is-hidden");
-    const arr = await getData("users");
+    loader.classList.remove('is-hidden');
+    const arr = await getData('users');
 
     addMarkup(usersWrapperEl, markupUsers(arr));
   } catch (err) {
     console.log(err.message);
   } finally {
-    loader.classList.add("is-hidden");
+    loader.classList.add('is-hidden');
   }
+}
+
+usersWrapperEl.addEventListener('click', onClick);
+
+function onClick(event) {
+  const id = event.target.closest('tr').dataset.userid;
+  location.href = `user.html?userid=${id}`;
 }
 
 onLoaded();
