a001d3a942f2bb0bb4abcfdcbf4dc90c4200851f
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-01-27 10:17:07 +0200

    start project

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,3 @@
 import axios from "axios";
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
-axios.defaults.baseURL = BASE_URL;
-
-async function getData(params = "") {
-  const { data } = await axios.get(params);
-
-  return data;
-}
-
-export { getData };
--- src/refs/index.js
+++
@@ -1,9 +0,0 @@
-const usersWrapperEl = document.querySelector(".js-tbody");
-const loader = document.querySelector(".loader");
-
-const userEl = document.querySelector('.js-user-table')
-const albumEl = document.querySelector('.js-album')
-
-
-const galleryEl = document.querySelector('.js-list-song')
-export { usersWrapperEl, loader, userEl, albumEl, galleryEl };
